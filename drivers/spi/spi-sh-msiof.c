renesas-rcar/linux-bsp

spi: sh-msiof: Add MSIOF module clock changing processing for R-Car Gen3
This patch adds processing to set MSIOF module clock.

To use this processing, make the following preparations to the device tree.
1.Add a pointer to the "msiof_ref_clk" node to the clocks property.

2.Define the module clock to Hz in the "clock-frequency" property
  of the msiof_ref_clk node. Please set a value based on electrical
  characteristics.

/* module clock */
&msiof_ref_clk {
	clock-frequency = <133333333>;
};

Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>

drivers/spi/spi-sh-msiof.c
@@ -1364,6 +1364,8 @@ static int sh_msiof_spi_probe(struct platform_device *pdev)
 	const struct sh_msiof_chipdata *chipdata;
 	struct sh_msiof_spi_info *info;
 	struct sh_msiof_spi_priv *p;
+	struct clk *ref_clk;
+	u32 clk_rate = 0;
 	int i;
 	int ret;
 	const struct soc_device_attribute *attr;
@@ -1469,6 +1471,17 @@ static int sh_msiof_spi_probe(struct platform_device *pdev)
 	if (ret < 0) {
 		dev_err(&pdev->dev, "devm_spi_register_controller error.\n");
 		goto err2;
+	}
+
+	/* MSIOF module clock setup */
+	ref_clk = devm_clk_get(&pdev->dev, "msiof_ref_clk");
+	if (!IS_ERR(ref_clk)) {
+		clk_rate = clk_get_rate(ref_clk);
+		if (clk_rate) {
+			clk_prepare_enable(p->clk);
+			clk_set_rate(p->clk, clk_rate);
+			clk_disable_unprepare(p->clk);
+		}
 	}
 
 	return 0;

kernelorg-mirror/linux_kernel_git_kuninori.morimoto.gx_linux

    clock-frequency = <133333333>;
Signed-off-by: Duc Nguyen <duc.nguyen.ub@renesas.com>
Signed-off-by: Thanh Quan <thanh.quan.xn@renesas.com>

drivers/spi/spi-sh-msiof.c
@@ -1292,6 +1292,7 @@ static int sh_msiof_spi_probe(struct platform_device *pdev)
 	struct sh_msiof_spi_info *info;
 	struct sh_msiof_spi_priv *p;
 	unsigned long clksrc;
+	struct clk *ref_clk;
 	int i;
 	int ret;
 
@@ -1335,6 +1336,17 @@ static int sh_msiof_spi_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "cannot get clock\n");
 		ret = PTR_ERR(p->clk);
 		goto err1;
+	}
+
+	/* MSIOF module clock setup */
+	ref_clk = devm_clk_get(&pdev->dev, "msiof_ref_clk");
+	if (!IS_ERR(ref_clk)) {
+		clksrc = clk_get_rate(ref_clk);
+		if (clksrc) {
+			clk_prepare_enable(p->clk);
+			clk_set_rate(p->clk, clksrc);
+			clk_disable_unprepare(p->clk);
+		}
 	}
 
 	i = platform_get_irq(pdev, 0);